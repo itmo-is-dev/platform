0b3dbc6c83fa3aefbcf366b398e627f926913af6
ronimizy <ronimizy@outlook.com>
2023-10-29 21:38:38 +0300

    chore: added timeoutChunkSpan to ChunkAsync

--- src/Itmo.Dev.Platform.Common/Extensions/AsyncEnumerableExtensions.cs
+++ src/Itmo.Dev.Platform.Common/Extensions/AsyncEnumerableExtensions.cs
@@ -4,13 +4,51 @@ namespace Itmo.Dev.Platform.Common.Extensions;
 
 public static class AsyncEnumerableExtensions
 {
+    /// <summary>
+    ///     Chunks an async enumerable
+    /// </summary>
+    /// <param name="enumerable">Chunked async enumerable</param>
+    /// <param name="count">Chunk max size (>1)</param>
+    /// <param name="timeout">
+    ///     Wait time for when enumerator is not completed.
+    ///     If enumerator would not complete before that time span,
+    ///     existing values for current chunk are yielded. 
+    /// </param>
+    /// <typeparam name="T">Type of chunked values</typeparam>
+    /// <returns>Async enumerable of chunks</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown when <see cref="count"/> is less than or equal to 1,
+    ///     or <see cref="timeout"/> is less than <see cref="timeoutChunkSpan"/>
+    /// </exception>
     public static IAsyncEnumerable<IReadOnlyList<T>> ChunkAsync<T>(
         this IAsyncEnumerable<T> enumerable,
         int count,
         TimeSpan timeout)
     {
+        return enumerable.ChunkAsync(count, timeout, timeoutChunkSpan: timeout);
+    }
+
+    /// <inheritdoc cref="ChunkAsync{T}(System.Collections.Generic.IAsyncEnumerable{T},int,System.TimeSpan)"/>
+    /// <param name="timeoutChunkSpan">
+    ///     Time span that <see cref="timeout"/> are split into.
+    ///     When enumerator completes in any of  <see cref="timeoutChunkSpan"/> spans, its result 
+    ///     continues to process.
+    /// </param>
+    public static IAsyncEnumerable<IReadOnlyList<T>> ChunkAsync<T>(
+        this IAsyncEnumerable<T> enumerable,
+        int count,
+        TimeSpan timeout,
+        TimeSpan timeoutChunkSpan)
+    {
         if (count <= 1)
             throw new ArgumentOutOfRangeException(nameof(count));
+
+        if (timeout < timeoutChunkSpan)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(timeoutChunkSpan),
+                "Timeout chunk span cannot be greater than timeout");
+        }
 
         return AsyncEnumerable.Create(ChunkInternal);
 
@@ -33,9 +71,17 @@ public static class AsyncEnumerableExtensions
                     enumeratorTask = enumerator.MoveNextAsync();
                     var awaiter = enumeratorTask.GetAwaiter();
 
-                    if (awaiter.IsCompleted is false && timeout > TimeSpan.Zero)
+                    var remainingDelay = timeout;
+
+                    while (awaiter.IsCompleted is false && remainingDelay > TimeSpan.Zero)
                     {
-                        await Task.Delay(timeout, cancellationToken);
+                        var delay = remainingDelay <= timeoutChunkSpan
+                            ? remainingDelay
+                            : timeoutChunkSpan;
+
+                        await Task.Delay(delay, cancellationToken);
+
+                        remainingDelay -= delay;
                     }
 
                     if (awaiter.IsCompleted is false && buffer.Count > 0)

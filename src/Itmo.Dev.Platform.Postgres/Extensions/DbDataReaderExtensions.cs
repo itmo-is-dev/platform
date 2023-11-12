fb5a006e8758742e4b46d563a8c56440f6538e5d
ronimizy <ronimizy@outlook.com>
2023-11-12 21:39:30 +0300

    feat: added json reader extension

--- src/Itmo.Dev.Platform.Postgres/Extensions/DbDataReaderExtensions.cs
+++ src/Itmo.Dev.Platform.Postgres/Extensions/DbDataReaderExtensions.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System.Data.Common;
 
 namespace Itmo.Dev.Platform.Postgres.Extensions;
@@ -6,4 +7,15 @@ public static class DbDataReaderExtensions
 {
     public static string? GetNullableString(this DbDataReader reader, int ordinal)
         => reader.IsDBNull(ordinal) ? null : reader.GetString(ordinal);
+
+    public static T GetFieldValue<T>(this DbDataReader reader, int ordinal, JsonSerializerSettings serializerSettings)
+    {
+        var serialized = reader.GetString(ordinal);
+        var deserialized = JsonConvert.DeserializeObject<T>(serialized, serializerSettings);
+
+        if (deserialized is null)
+            throw new ArgumentException($"Value at position {ordinal} is not a valid {typeof(T)} json");
+
+        return deserialized;
+    }
 }

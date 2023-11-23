6f2545f3b51ac77fce3710321e91e133db8b056c
ronimizy <ronimizy@outlook.com>
2023-11-24 00:03:03 +0300

    chore: added connection provider loggin

--- src/Itmo.Dev.Platform.Postgres/Connection/PostgresConnectionProvider.cs
+++ src/Itmo.Dev.Platform.Postgres/Connection/PostgresConnectionProvider.cs
@@ -1,5 +1,6 @@
 using Itmo.Dev.Platform.Postgres.Models;
 using Itmo.Dev.Platform.Postgres.Plugins;
+using Microsoft.Extensions.Logging;
 using Npgsql;
 
 namespace Itmo.Dev.Platform.Postgres.Connection;
@@ -7,9 +8,14 @@ namespace Itmo.Dev.Platform.Postgres.Connection;
 internal class PostgresConnectionProvider : IPostgresConnectionProvider, IAsyncDisposable, IDisposable
 {
     private readonly Lazy<Task<NpgsqlConnection>> _connection;
+    private readonly ILogger<PostgresConnectionProvider> _logger;
 
-    public PostgresConnectionProvider(PostgresConnectionString connectionString, IEnumerable<IDataSourcePlugin> plugins)
+    public PostgresConnectionProvider(
+        PostgresConnectionString connectionString,
+        IEnumerable<IDataSourcePlugin> plugins,
+        ILogger<PostgresConnectionProvider> logger)
     {
+        _logger = logger;
         _connection = new Lazy<Task<NpgsqlConnection>>(async () =>
         {
             var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString.Value);
@@ -20,6 +26,8 @@ internal class PostgresConnectionProvider : IPostgresConnectionProvider, IAsyncD
             }
 
             var dataSource = dataSourceBuilder.Build();
+            
+            _logger.LogTrace("Opening connection");
 
             return await dataSource.OpenConnectionAsync();
         });
@@ -33,7 +41,12 @@ internal class PostgresConnectionProvider : IPostgresConnectionProvider, IAsyncD
     public async ValueTask DisposeAsync()
     {
         if (_connection.IsValueCreated is false)
+        {
+            _logger.LogTrace("Connection was not created");
             return;
+        }
+        
+        _logger.LogTrace("Disposing connection");
 
         var connection = await _connection.Value;
         await connection.DisposeAsync();

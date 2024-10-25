002addbbc4676ddba57e4d91e2cb21c072aa70b4
ronimizy <ronimizy@outlook.com>
2024-10-25 20:11:11 +0300

    fix: naming

--- .github/workflows/publish-find-changed-packages.sh
+++ .github/workflows/publish-find-changed-packages.sh
@@ -4,7 +4,7 @@ package_projects=$(echo "$PACKAGE_PROJECTS_JSON" | jq -r 'values[]' | tr '\n' ' 
 
 for package_project in ${package_projects}
 do
-  dependent_paths=$(.github/workflows/publish-new-find-csproj-dependencies.sh "${package_project}" | sort | uniq)
+  dependent_paths=$(.github/workflows/publish-find-csproj-dependencies.sh "${package_project}" | sort | uniq)
 
   for dependent_path in ${dependent_paths}
   do

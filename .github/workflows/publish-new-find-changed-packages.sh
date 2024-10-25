33d5a5c30ca1081b81b7174794b050feea638a5a
ronimizy <ronimizy@outlook.com>
2024-10-25 19:52:35 +0300

    fix: script

---
+++ .github/workflows/publish-new-find-changed-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+package_projects=$(echo "$PACKAGE_PROJECTS_JSON" | jq -r 'values[]' | tr '\n' ' ')
+
+for package_project in ${package_projects}
+do
+  dependent_paths=$(.github/workflows/publish-new-find-csproj-dependencies.sh "${package_project}" | sort | uniq)
+
+  for dependent_path in ${dependent_paths}
+  do
+    changes_count=$(echo "$ALL_CHANGED_FILES" | grep -Ec "${dependent_path}")
+
+    if [[ ! "$changes_count" -eq 0 ]]
+    then
+      echo "${package_project}"
+      break
+    fi
+  done
+done

3d958f56c5e58c7332bc41a9e2a132449b145ed9
ronimizy <ronimizy@outlook.com>
2024-10-25 20:45:21 +0300

    chore: determining package update

--- .github/workflows/publish-find-changed-packages.sh
+++ .github/workflows/publish-find-changed-packages.sh
@@ -4,9 +4,9 @@ package_projects=$(echo "$PACKAGE_PROJECTS_JSON" | jq -r 'values[]' | tr '\n' ' 
 
 for package_project in ${package_projects}
 do
-  dependent_paths=$(.github/workflows/publish-find-csproj-dependencies.sh "${package_project}" | sort | uniq)
+  project_references=$(.github/workflows/publish-find-project-references.sh "${package_project}" | sort | uniq)
 
-  for dependent_path in ${dependent_paths}
+  for dependent_path in ${project_references}
   do
     changes_count=$(echo "$ALL_CHANGED_FILES" | grep -Ec "${dependent_path}")
 
@@ -17,3 +17,27 @@ do
     fi
   done
 done
+
+changed_packages=$(git --no-pager diff HEAD^1 HEAD Directory.Packages.props)
+changed_packages=$(echo "${changed_packages}" | grep '^\+ ' | grep -Eo 'Include="[a-zA-Z.]*"' | grep -Eo '".*"' | tr -d '"' | tr '\n' ' ')
+
+if [[ ! -z "${changed_packages}" ]]
+then
+  for package_project in ${package_projects}
+  do
+    package_references=$(.github/workflows/publish-find-package-references.sh "${package_project}" | sort | uniq)
+    
+    for package_reference in ${package_references}
+    do
+      changes_count=$(echo "${changed_packages}" | grep -Ec "${package_reference}")
+      
+      if [[ ! "$changes_count" -eq 0 ]]
+      then
+        echo "${package_project}"
+        break
+      fi
+      
+    done
+    
+  done
+fi

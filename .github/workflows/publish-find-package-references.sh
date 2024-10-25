0237c0fcb6fe9648099c65d084e35e07a64c61dc
ronimizy <ronimizy@outlook.com>
2024-10-25 22:13:23 +0300

    fix: csproj pattern

--- .github/workflows/publish-find-package-references.sh
+++ .github/workflows/publish-find-package-references.sh
@@ -5,7 +5,7 @@ project_references=$(.github/workflows/publish-find-project-references.sh "${pro
 
 for project_reference in ${project_references}
 do
-  project_csproj=$(ls "${project_reference}" | grep -E 'csproj')
+  project_csproj=$(ls "${project_reference}" | grep -E 'csproj$')
   project_csproj="$project_reference"/"$project_csproj"
   
   grep PackageReference "${project_csproj}" | grep -Eo 'Include=".*"' | grep -Eo '".*"' | tr -d '"'

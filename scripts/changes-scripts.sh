76a68fdbe87d91968debe91fa3b87f36a6fcd277
ronimizy <ronimizy@outlook.com>
2024-10-26 23:46:08 +0300

    fix: pipe

--- scripts/changes-scripts.sh
+++ scripts/changes-scripts.sh
@@ -7,8 +7,11 @@ function find_changed_projects {
   
   find_package_projects | while read -r project
   do
+    >&2 echo 'checking changes for project = '"$project"
+    
     echo "$project" | find_project_references_by_short_name | short_name_to_dir_path | while IFS= read -r dependency
     do
+      >&2 echo "\t"'checking changes for project dependency = '"$dependency"
       changed=$(echo "$changes" | grep -Ec "$dependency")
       
       if [[ ! "$changed" -eq 0 ]]
@@ -25,8 +28,11 @@ function find_changed_dependencies {
     
     find_package_projects | while read -r project
     do
+      >&2 echo 'checking package changes for project = '"$project"
       echo "$project" | find_package_references_by_short_name | while read -r package_referece
       do
+        >&2 echo "\t"'checking changes for package = '"$package_referece"
+        
         changed=$(echo "$changed_dependencies" | grep -Ec "$package_referece")
         
         if [[ ! "$changed" -eq 0 ]]

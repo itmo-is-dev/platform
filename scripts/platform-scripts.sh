066f98abff3cc5674e330f500818c697a308371d
ronimizy <ronimizy@outlook.com>
2024-10-30 21:02:29 +0300

    chore: serilog enrichers

--- scripts/platform-scripts.sh
+++ scripts/platform-scripts.sh
@@ -27,3 +27,21 @@ function change_package_patch_version {
   fi
 
 }
+
+function get_package_version {
+    short_name="$1"
+    
+    csproj_path=$(echo "$short_name" | short_name_to_csproj_path)
+    
+    platform_version_line_number=$(grep -n '<PropertyGroup Label="PlatformVersion">' "$csproj_path" | awk -F : '{ print $1 }')
+    
+    major_version_line_number=$(( platform_version_line_number + 1 ))
+    minor_version_line_number=$(( platform_version_line_number + 2 ))
+    patch_version_line_number=$(( platform_version_line_number + 3 ))
+    
+    major_version=$(sed -n "$major_version_line_number"p "$csproj_path" | grep -Eo '[0-9]+')
+    minor_version=$(sed -n "$minor_version_line_number"p "$csproj_path" | grep -Eo '[0-9]+')
+    patch_version=$(sed -n "$patch_version_line_number"p "$csproj_path" | grep -Eo '[0-9]+')
+    
+    echo "$major_version"."$minor_version"."$patch_version"
+}

fe17a8991a746d230111ad0b7aaf1f7c14a56c29
ronimizy <ronimizy@outlook.com>
2025-05-28 01:39:51 +0300

    GetLatestByKey

--- src/Itmo.Dev.Platform.Kafka/Extensions/ConsumerMessageExtensions.cs
+++ src/Itmo.Dev.Platform.Kafka/Extensions/ConsumerMessageExtensions.cs
@@ -23,4 +23,12 @@ public static class ConsumerMessageExtensions
     {
         return messages.GetLatestBy(x => x.Key);
     }
+
+    public static IEnumerable<IKafkaConsumerMessage<TKey, TValue>> GetLatestByKey<TKey, TValue, TSelector>(
+        this IEnumerable<IKafkaConsumerMessage<TKey, TValue>> messages,
+        Func<TKey, TSelector> selector)
+        where TSelector : IComparable<TSelector>
+    {
+        return messages.GetLatestBy(message => selector.Invoke(message.Key));
+    }
 }

7de41851f39eebe64f630d5f1ec5e642e6521377
ronimizy <ronimizy@outlook.com>
2025-05-28 03:42:47 +0300

    fix: GetLatestBy

--- src/Itmo.Dev.Platform.Kafka/Extensions/ConsumerMessageExtensions.cs
+++ src/Itmo.Dev.Platform.Kafka/Extensions/ConsumerMessageExtensions.cs
@@ -7,19 +7,31 @@ public static class ConsumerMessageExtensions
     public static IEnumerable<IKafkaConsumerMessage<TKey, TValue>> GetLatestBy<TKey, TValue, TSelector>(
         this IEnumerable<IKafkaConsumerMessage<TKey, TValue>> messages,
         Func<IKafkaConsumerMessage<TKey, TValue>, TSelector> selector)
-        where TSelector : IComparable<TSelector>
+        where TSelector : IEquatable<TSelector>
     {
-        return messages.GroupBy(
-            selector,
-            (_, group) => group
+        return messages.GroupBy(selector, GetLatestMessageInGroup);
+
+        IKafkaConsumerMessage<TKey, TValue> GetLatestMessageInGroup(
+            TSelector key,
+            IEnumerable<IKafkaConsumerMessage<TKey, TValue>> selectorGroup)
+        {
+            return selectorGroup
+                .GroupBy(message => message.Partition.Value, GetLatestMessageInPartition)
                 .OrderByDescending(x => x.Timestamp)
-                .ThenByDescending(x => x.Offset)
-                .First());
+                .First();
+        }
+
+        IKafkaConsumerMessage<TKey, TValue> GetLatestMessageInPartition(
+            int partition,
+            IEnumerable<IKafkaConsumerMessage<TKey, TValue>> partitionGroup)
+        {
+            return partitionGroup.MaxBy(x => x.Offset)!;
+        }
     }
 
     public static IEnumerable<IKafkaConsumerMessage<TKey, TValue>> GetLatestByKey<TKey, TValue>(
         this IEnumerable<IKafkaConsumerMessage<TKey, TValue>> messages)
-        where TKey : IComparable<TKey>
+        where TKey : IEquatable<TKey>
     {
         return messages.GetLatestBy(x => x.Key);
     }
@@ -27,7 +39,7 @@ public static class ConsumerMessageExtensions
     public static IEnumerable<IKafkaConsumerMessage<TKey, TValue>> GetLatestByKey<TKey, TValue, TSelector>(
         this IEnumerable<IKafkaConsumerMessage<TKey, TValue>> messages,
         Func<TKey, TSelector> selector)
-        where TSelector : IComparable<TSelector>
+        where TSelector : IEquatable<TSelector>
     {
         return messages.GetLatestBy(message => selector.Invoke(message.Key));
     }
